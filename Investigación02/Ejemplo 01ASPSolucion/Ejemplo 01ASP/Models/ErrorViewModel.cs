53f75dfa6be5fbe2dacd17b8f69142adfe1c0409
Bryan Peña <bryanlexis30@gmail.com>
2025-09-04 16:52:51 -0600

    Eliminacion de archivo inncesario

--- Investigación02/Ejemplo 01ASPSolucion/Ejemplo 01ASP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ejemplo_01ASP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
