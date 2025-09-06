a1efb45dd0954742c8012d4d555a467205e74116
yassir <yassirmelendezaaviles14@gmail.com>
2025-09-05 21:36:56 -0300

    implementacion
    se implemento la funcion eliminar libro

--- Investigación02/LibreríaSolucion/Librería/Controllers/LibrosController.cs
+++ Investigación02/LibreríaSolucion/Librería/Controllers/LibrosController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Runtime.CompilerServices;
 using Librería.Models;
 
 namespace Librería.Controllers
@@ -11,8 +10,8 @@ namespace Librería.Controllers
             new() { Id = 1, NameBook = "Cien años de soledad", NameAuthor = "Gabriel García Márquez", anioPubli = 1967 },
             new() { Id = 2, NameBook = "1984", NameAuthor = "George Orwell", anioPubli = 1949 },
             new() { Id = 3, NameBook = "Orgullo y prejuicio (Pride and Prejudice)", NameAuthor = "Jane Austen", anioPubli = 1813 },
-            new() { Id = 4, NameBook = "El Señor de los Anillos: La Comunidad del Anillo (The Fellowship of the Ring)", NameAuthor = "J.R.R. Tolkien", anioPubli = 1954 }, 
-            new() { Id = 5, NameBook = "La casa de los espíritus", NameAuthor = "Isabel Allende", anioPubli = 1982}
+            new() { Id = 4, NameBook = "El Señor de los Anillos: La Comunidad del Anillo (The Fellowship of the Ring)", NameAuthor = "J.R.R. Tolkien", anioPubli = 1954 },
+            new() { Id = 5, NameBook = "La casa de los espíritus", NameAuthor = "Isabel Allende", anioPubli = 1982 }
         ];
 
         public IActionResult Directorio()
@@ -45,6 +44,29 @@ namespace Librería.Controllers
                 return RedirectToAction("Directorio");
             }
             return View(libro);
+        }
+
+        // Acción para mostrar la vista de confirmación de eliminación
+        public IActionResult Delete(int Id)
+        {
+            var libro = libros.FirstOrDefault(libro => libro.Id == Id);
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            return View(libro);
+        }
+
+        // Acción para confirmar y eliminar el libro
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int Id)
+        {
+            var libro = libros.FirstOrDefault(libro => libro.Id == Id);
+            if (libro != null)
+            {
+                libros.Remove(libro);
+            }
+            return RedirectToAction("Directorio");
         }
     }
 }
