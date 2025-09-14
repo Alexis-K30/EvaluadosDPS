c50ce4f4821aa9a3a28ff6a523ea0981400e3183
Bryan Peña <bryanlexis30@gmail.com>
2025-09-14 15:51:55 -0600

    Creación
    Creación inicial del proyecto

---
+++ Proyecto de Cátedra Fase 2/CotizacionWebSolucion/CotizacionWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CotizacionWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
